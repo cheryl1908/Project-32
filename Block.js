2d100fab2154fdc5b27c6cb793da7a0ebf682f41
cheryl1908 <73298909+cheryl1908@users.noreply.github.com>
2021-04-05 16:52:59 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,46 @@
+class Block{
+  constructor(x, y, width, height) {
+      var options = {
+         
+          restitution :0.4,
+          friction :0.0,
+          //isStatic:true
+         
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.color=color(random(0,255),random(0,255),random(0,255));
+      this.width = width;
+      this.height = height;
+      this.Visibility=255;
+      World.add(world, this.body);
+    }
+
+
+score(){
+  if(this.Visibility<0 && this.Visibility >-105){
+    score++;
+  }
+}
+
+    display(){
+      if(this.body.speed < 3){
+        var angle = this.body.angle;
+        var pos= this.body.position;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        fill(this.color);
+        rectMode(CENTER);
+        rect(0,0,this.width, this.height);
+        pop();
+    }
+     else{
+       World.remove(world, this.body);
+       push();
+       this.Visiblity = this.Visiblity - 5;
+       tint(255,this.Visiblity);
+       pop();
+     }
+    }
+
+}
